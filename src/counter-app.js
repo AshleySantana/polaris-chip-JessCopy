63b0ea523f4dd6f3cff4fd7c7543958d2a85b9a4
jgeevarghese <89812287+jgeevarghese@users.noreply.github.com>
2024-02-20 22:11:22 -0500

    it works!

--- src/counter-app.js
+++ src/counter-app.js
@@ -12,12 +12,21 @@ export class CounterApp extends LitElement {
     return 'counter-app';
   }
 
+  static get properties() {
+    return {
+      title: { type: String},
+      value: { type: Number, reflect:true},
+      min: {type: Number},
+      max: {type: Number},
+    };
+  }
+
   constructor() {
     super();
     this.title="Basic Counter";
     this.value=0;
-    this.name="Cool Counter"
-    this.num=0;
+    this.min=0;
+    this.max=10;
     
   }
 
@@ -31,7 +40,6 @@ export class CounterApp extends LitElement {
       margin: 5%;
       font-size: 30px;
     }
-
 
     .add {
       background: blue;
@@ -47,95 +55,69 @@ export class CounterApp extends LitElement {
       font-size: 40px;
     }
 
-    .cool-counter {
-      background-color: purple;
-      width: 400px;
-      padding: 5%;
-      margin: 5%;
-      font-size: 30px;
-    }
-
-    .increase {
-      background: blue;
-      width: 100px;
-      border-radius: 20px;
-      font-size: 40px;
-    }
-
-    .decrease{
-      background: blue;;
-      width: 100px;
-      border-radius: 20px;
-      font-size: 40px;
-    }
-
-    .increase:hover {
+    .add:hover {
       background-color: grey;
       border: 1px solid black;
     }
     
-    .decrease:hover {
+    .subtract:hover {
       background-color: grey;
       border: 1px solid black;
     }
+
     `;
   }
 
   add(){
-    if(this.value < 20){
+    if(this.value < this.max){
       this.value += 1;
     }
   }
 
   subtract(){
-    if(this.value > 0){
+    if(this.value > this.min){
       this.value -= 1;
     }
   }
 
-  increase(){
-    if(this.num < 20) {
-      this.num += 1;
+  makeItRain() {
+    import("@lrnwebcomponents/multiple-choice/lib/confetti-container.js").then(
+      (module) => {
+        setTimeout(() => {
+          this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
+        }, 0);
+      }
+    );
+  }
+ 
+  updated(changedProperties) {
+    if(changedProperties.has(value)) {
+      if(this.value == 21) {
+        this.makeItRain();
+      }
     }
   }
-
-  decrease(){
-    if(this.num > 0){
-      this.num -= 1;
-    }
-
   
-  }
 
   render() {
+    var color = "black";
+    if(this.value === this.min) color = "green";
+    if(this.value === 18) color = "yellow";
+    if(this.value === 21) color = "blue";
+    if(this.value === this.max) color = "red";
+
     return html`
     <div class="counter-app">
       <h3 class="header">${this.title}</h3>
-      <h2 class="value">${this.value}</h2>
-      <button class="add" @click="${this.add}">+</button>
-      <button class="subtract" @click="${this.subtract}">-</button>
-    </div>
-
-    <div class="cool-counter">
-      <h3 class="header">${this.name}</h3>
-      <h2 class="value">${this.num}</h2>
-      <button class="increase" @click="${this.increase}">+</button>
-      <button class="decrease" @click="${this.decrease}">-</button>
+      <h2 class="value" style="color: ${color}">${this.value}</h2>
+      <button class="add" @click="${this.add}" ?disabled="${this.max===this.value}">+</button>
+      <button class="subtract" @click="${this.subtract}" ?disabled="${this.min===this.value}">-</button>
     </div>
 
   `;
-
-   
   }
 
-  static get properties() {
-    return {
-      title: { type: String},
-      value: { type: Number},
-      name: { type: String},
-      num: { type: Number}  
-    };
-  }
+  
 }
 
 globalThis.customElements.define(CounterApp.tag, CounterApp);
--- src/my-card.js
+++ src/my-card.js
@@ -135,9 +135,9 @@ export class MyCard extends LitElement {
 
 :host([fancy]) {
 display: block;
-  background-color: pink;
-  border: 2px solid fuchsia;
-  box-shadow: 10px 5px 5px red;
+background-color: pink;
+border: 2px solid fuchsia;
+box-shadow: 10px 5px 5px red;
 }
     `;
   }
